41f4896c3a651e93aa17a369cd62fada9a7ca2f0
cmr201966 <cmr201966@gmail.com>
2025-05-09 14:49:13 -0400

    ultimo

--- src/App.vue
+++ src/App.vue
@@ -6,45 +6,85 @@ import TheWelcome from './components/TheWelcome.vue'
 <template>
   <v-app>
     <v-main>
-    <div class="caja"></div>
-    <div class="cajas">
-    <div class="cajagrande">cajagrande1</div>
-     <div class="cajagrande">cajagrande2</div>
-     </div>
-      <v-container>
-      </v-container>
+    <div class="cajagrande" style="width: 350px;">
+    <img src="c:\Users\m2G\Downloads\fcb.jpg" style="margin-left: 35px; width:280px; height: 180px;
+    margin-top: 20px; border-radius: 10px;"/>
+    <h1 style="position: absolute; left: 460px; top: 300px; font-size: 30px;"> FC BARCELONA </h1>
+    <div>
+    <p1 style="position: absolute; left: 460px; top: 350px; font-size: 20px; max-width: 350px;"> El FC Barcelona es un club de fútbol español. </p1>
+    </div>
+    <div></div>
+    <v-btn class="accion" style="height:50px;"> Acción </v-btn>
+    </div>
+    <div class="caja-pequeña">
+    <img src="c:\Users\m2G\Downloads\fcb2.jpg" style="width: 50px; height: 50px; margin-left: 25px; margin-top: 16px; border-radius: 50px;"/>
+     <h2 style="position: absolute; left: 540px; top: 535px;
+      font-size: 15px;"> FC BARCELONA </h2>
+      <p style="position: absolute; left: 540px; top: 552px;"> Información</p>
+      <v-menu offset-y>
+    <template #activator="{ props }">
+      <v-btn icon v-bind="props" style="position: absolute; left: 740px; bottom: 60px;" variant="plain">
+        <v-icon color="black">mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item @click="accion1">
+        <v-list-item-title>Opción 1</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="accion2">
+        <v-list-item-title>Opción 2</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+    </div>
     </v-main>
   </v-app>
 </template>
 
 <style>
 body {
-background-color:rgb(255, 255, 255);
+background-color:white;
 font-family: Verdana, sans-serif;
-padding: 20px;
+padding: 25px;
 }
-
-.caja{
-  background-color:black;
-  border: 2px solid #ccc;
-padding: 18px;
-margin-top: 20px;
-border-radius: 8px;
-width: 750px;
+.v-main{
+  border-radius: 16px;
+background-color: gray;
+}
+.accion{
+  background: linear-gradient(to right, blue,rgb(255, 0, 0));
+  width:340px;
+  margin-left: 5px;
+  margin-top: 170px;
+}
+.caja-pequeña{
+  background-color: white;
+  width:350px;
+  height:85px;
+  margin-left: 450px;
+  margin-top: 25px;
+  border-radius: 20px;
 }
 .cajagrande{
-border-radius:8px;
-border: 2px solid #ccc;
-width: 375px;
-padding: 180px;
-margin-top: 15px;
-vertical-align: middle;
+  background-color: white;
+border-radius:30px;
+border: 1px solid black;
+height:450px;
+margin-top: 40px;
+margin-left: 450px;
 }
+
 </style>
 <script setup>
 </script>
 
 <style scoped>
+.accion{
+  border-radius: 40px;
+  color: white;
+  background: linear-gradient(to right, blue,rgb(255, 0, 0));
+}
 header {
   line-height: 1.5;
 }
